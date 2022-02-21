0bb65227b6f2e54fc3f89858061c92213d2abd65
Thiru-01 <thirucandane@outlook.com>
2022-02-21 16:17:28 +0530

    setup commit

---
+++ lib/controller/datacontroller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class DataController extends GetxController {
+  var token = ''.obs;
+  getToken(String userToken) {
+    print(token);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:amboo/constant.dart';
+import 'package:amboo/controller/datacontroller.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'pages/songpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +14,107 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'AmBoo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: primaryColor, scaffoldBackgroundColor: Colors.white),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
+  final formKey = GlobalKey<FormState>();
+  DataController tokenController = Get.put(DataController());
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.09,
+                  width: double.infinity,
+                  child: Form(
+                    key: formKey,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            textInputAction: TextInputAction.go,
+                            validator: (input) {
+                              if (input!.isEmpty) {
+                                return "Token can't be empty";
+                              }
+                              return null;
+                            },
+                            onFieldSubmitted: (_token) {
+                              if (formKey.currentState!.validate()) {
+                                formKey.currentState!.save();
+                              }
+                            },
+                            onSaved: (_token) {
+                              tokenController.getToken(_token ?? 'empty');
+                              Get.off(() => const SongPage());
+                            },
+                            decoration: InputDecoration(
+                                prefix: SizedBox(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.05),
+                                hintText: 'Enter the token',
+                                contentPadding: const EdgeInsets.only(
+                                  top: 0,
+                                  bottom: 0,
+                                  left: 0,
+                                ),
+                                suffixIcon: const Icon(FontAwesomeIcons.qrcode),
+                                border: const OutlineInputBorder()),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.03,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              formKey.currentState!.save();
+                            }
+                          },
+                          style: TextButton.styleFrom(
+                              side: const BorderSide(
+                                  color: primaryColor, width: 2),
+                              backgroundColor: Colors.white,
+                              minimumSize: Size(
+                                  MediaQuery.of(context).size.width * 0.1,
+                                  MediaQuery.of(context).size.height * 0.053),
+                              elevation: 0),
+                          child: const Text(
+                            'Connect',
+                            style: TextStyle(color: primaryColor),
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
+              SvgPicture.asset('assets/svg/1.svg')
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
